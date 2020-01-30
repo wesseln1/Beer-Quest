09c8f52fd7aec038e817568b375798517a278fd2
unknown <nwessel429@gmail.com>
2020-01-30 09:06:49 -0600

    added gitignore

---
+++ Beer-Quest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Beer_Quest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
