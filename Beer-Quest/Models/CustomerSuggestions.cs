e91916741a47af6800237742ee7a8e0cbec66743
unknown <nwessel429@gmail.com>
2020-04-11 18:00:11 -0500

    added suggestion class

---
+++ Beer-Quest/Models/CustomerSuggestions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beer_Quest.Models
+{
+    public class CustomerSuggestions
+    {
+        public int Id { get; set; }
+        public string Suggestion { get; set; }
+        public string Name { get; set; }
+    }
+}
